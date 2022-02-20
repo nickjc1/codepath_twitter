a7497350d15576b683c66e0e0e66ca4bf291b10d
Chao Jiang <cjiang@Chaos-MBP.hsd1.ms.comcast.net>
2022-02-20 09:26:42 -0600

    App initialized; icon assets added

---
+++ Twitter/Twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Chao Jiang on 2/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
