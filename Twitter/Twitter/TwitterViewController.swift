164509269a5b6880ef4e6f806ed8dc9643db8e99
Chao Jiang <cjiang@Chaos-MBP.hsd1.ms.comcast.net>
2022-02-20 11:04:05 -0600

    loginButton configured, twitterViewController created

---
+++ Twitter/Twitter/TwitterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TwitterViewController.swift
+//  Twitter
+//
+//  Created by Chao Jiang on 2/20/22.
+//
+
+import UIKit
+
+class TwitterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Twitter"
+        self.view.backgroundColor = .systemRed
+
+    }
+    
+}
