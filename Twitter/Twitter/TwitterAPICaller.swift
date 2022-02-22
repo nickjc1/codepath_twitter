e76f0bff8f6644200fb71279878cfcfaec023721
Chao Jiang <cjiang@Chaos-MBP.hsd1.ms.comcast.net>
2022-02-21 21:29:44 -0600

    restarted the project by using the provided twitter_ios_starter pkg

--- Twitter/Twitter/TwitterAPICaller.swift
+++ Twitter/Twitter/TwitterAPICaller.swift
@@ -1,14 +1,15 @@
 //
-//  TwitterAPICaller.swift
+//  APIManager.swift
 //  Twitter
 //
-//  Created by Chao Jiang on 2/20/22.
+//  Created by Dan on 1/3/19.
+//  Copyright © 2019 Dan. All rights reserved.
 //
 
 import UIKit
 import BDBOAuth1Manager
 
-class TwitterAPICaller: BDBOAuth1SessionManager {
+class TwitterAPICaller: BDBOAuth1SessionManager {    
     static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
@@ -64,4 +65,3 @@ class TwitterAPICaller: BDBOAuth1SessionManager {
     }
     
 }
-
